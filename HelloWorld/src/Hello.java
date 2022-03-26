f0c1c8949566b7b23ab1a6a2faff18facdfa756b
Robert Resch <rob@Roberts-MacBook-Pro.local>
2022-03-26 12:57:19 -0600

    Hello World

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,30 +3,7 @@ public class Hello {
     public static void main( String[] args ) {
 
         System.out.println("Hello, Rob!");
-
-        // initialize int variables
-        int myFirstNumber, mySecondNumber, myThirdNumber, myTotal, myFinalOne;
-
-        // set the values to the declared ints above
-        myFirstNumber = (10 + 5) + (2*10);
-        mySecondNumber = 12;
-        myThirdNumber = myFirstNumber * 2;
-
-        // find the total
-        myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
-
-        // Challenge to find 1000 less the value of myTotal
-        myFinalOne = 1000 - myTotal;
-
-
-        System.out.println("I am " + myFinalOne + " today.");
-
-
-
-
-
         
-
     }
 
 }
