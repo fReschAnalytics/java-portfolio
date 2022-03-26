620f059f66eb63889076b3124083a46f15b31540
Robert Resch <rob@Roberts-MacBook-Pro.local>
2022-03-26 16:20:43 -0600

    floats and doubles

---
+++ FloatDouble/src/com/freschanalytics/Main.java
@@ -0,0 +1,20 @@
+package com.freschanalytics;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // floats
+        float myMinFloat = Float.MIN_VALUE;
+        float myMaxFloat = Float.MAX_VALUE;
+
+        System.out.println("The minimum float value is: " + myMinFloat);
+        System.out.println("The maximum float value is: " + myMaxFloat);
+
+        //doubles
+        double myMinDouble = Double.MIN_VALUE;
+        double myMaxDouble = Double.MAX_VALUE;
+
+        System.out.println("The minimum double value is: " + myMinDouble);
+        System.out.println("The maximum double value is: " + myMaxDouble);
+    }
+}
